0305528bdb8222d99b233f12c83849211836a21e
anuzbvbmaniac <anuzbvbmaniac123@gmail.com>
2021-06-01 04:03:24 +0545

    Changes: Build Failing Fix with unused import removal.

--- src/assets/layouts/Navigation.js
+++ src/assets/layouts/Navigation.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { connect } from "react-redux";
 
 import { SearchIcon } from "@heroicons/react/solid";
